ae7d2630d3fd1a712c27774dab083eba8c38a718
pskalbhav1 <praseedhakalbhavi@gmail.com>
2021-06-22 14:57:11 +0530

    Updates

---
+++ precache-manifest.38b4d84c6502a2cef6abb3c749336709.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18800900f3501f21a4b1c54f0c65e53",
+    "url": "/Praseedha-Kalbhavi-Portfolio/index.html"
+  },
+  {
+    "revision": "8df6b70f93594e847a44",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/css/2.2885613b.chunk.css"
+  },
+  {
+    "revision": "7e67c8c4acdd87a56644",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/css/main.99b9f70a.chunk.css"
+  },
+  {
+    "revision": "8df6b70f93594e847a44",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/js/2.2447fa71.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/js/2.2447fa71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e67c8c4acdd87a56644",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/js/main.73f96194.chunk.js"
+  },
+  {
+    "revision": "925242cacf20f6183447",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/js/runtime-main.f52c91f9.js"
+  },
+  {
+    "revision": "2f7908af8e253df4363b24222035b7ed",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/1.2f7908af.jpg"
+  },
+  {
+    "revision": "531446324983c54b35d7a7497f5b018d",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/10.53144632.png"
+  },
+  {
+    "revision": "f530d2485829bf477287b8ad589af250",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/11.f530d248.jpeg"
+  },
+  {
+    "revision": "4c8aeba0328dd023c9ff79f6e1495aa7",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/12.4c8aeba0.jpeg"
+  },
+  {
+    "revision": "eef2807c9af3e14c785225d2c65a9752",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/2.eef2807c.jpg"
+  },
+  {
+    "revision": "4be4c7a0d97b9c40fda64ef2ab0d0562",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/3.4be4c7a0.jpg"
+  },
+  {
+    "revision": "44b8a90ebfd3e64f34ac8a77421f5c15",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/4.44b8a90e.png"
+  },
+  {
+    "revision": "f11df316fd9f91c6276b792ddfee3fba",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/5.f11df316.png"
+  },
+  {
+    "revision": "ac762f1ed5466bfd0df60db8b4989818",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/6.ac762f1e.jpeg"
+  },
+  {
+    "revision": "8cc2cbeaa86a0223c6e009265847f839",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/7.8cc2cbea.png"
+  },
+  {
+    "revision": "863dd9cba5d47162f33a1353c14818bd",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/8.863dd9cb.png"
+  },
+  {
+    "revision": "f2181b9741e2dd6e5624d1839d463ec2",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/9.f2181b97.jpeg"
+  },
+  {
+    "revision": "43dbf1b39319bbef9bfc2764d3a55bf3",
+    "url": "/Praseedha-Kalbhavi-Portfolio/static/media/Image.43dbf1b3.png"
+  }
+]);
